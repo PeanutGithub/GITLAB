8065af2d972cf23de3fd173e255858ca9a3d11c0
PeanutGithub <jittakorn.suwan@mail.kmutt.ac.th>
2022-09-22 23:24:33 +0700

    modified bloodgroup.c in A

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("Em,What's your Name? ");
+    printf("A,What's your Name? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("A,What is your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
